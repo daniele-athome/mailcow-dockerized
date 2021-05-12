133c13dce061574e2f8cafd2d1bfffe673b1a85d
Daniele Ricci <daniele@casaricci.it>
2021-05-12 20:40:55 +0200

    Fix string conversion in Python 3
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- data/Dockerfiles/calendarfilter/fuglu_caldav_plugin.py
+++ data/Dockerfiles/calendarfilter/fuglu_caldav_plugin.py
@@ -129,8 +129,8 @@ class CalendarPlugin(ScannerPlugin):
               attendees = [attendees]
 
             self.logger.debug("Attendees: %s", attendees)
-            for attendee in attendees:
-              attendee_mailto = attendee.decode("utf-8")
+            for attendee in attendees:  # type: icalendar.vCalAddress
+              attendee_mailto = attendee
               if not mailto_matches(attendee_mailto, suspect.from_address) and \
                 not mailto_matches(attendee_mailto, from_address):
                 self.logger.warning("Not accepting reply on behalf of another user! " +
@@ -200,10 +200,9 @@ class CalendarPlugin(ScannerPlugin):
     if event['uid'] != event_uid:
       raise ValueError("Event uid mismatch!")
 
-    for attendee in event['attendee']:  # type: icalendar.vCalAddress
-      attendee_mailto = str(attendee)
-      if attendee_mailto.casefold() == attendee_email.casefold():
-        return attendee
+    attendee = event['attendee']
+    if attendee.casefold() == attendee_email.casefold():
+      return attendee
 
     raise ValueError("Attendee not found: %s" % attendee_email)
 
